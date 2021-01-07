edc605efb384ab9c3f6c225fe57c06c573f86055
Manuel Quintero Cubillos <mquinteroc@psl.com.co>
2021-01-07 07:39:44 -0500

    Travis CI file generator

--- report_generator.js
+++ report_generator.js
@@ -1,18 +1,18 @@
 const reporter = require('cucumber-html-reporter');
 
 let options = {
-        theme: 'bootstrap',
-        jsonFile: 'test/cucumber-test-json/cucumber_report.json',
-        output: 'test/report/cucumber_report.html',
-        reportSuiteAsScenarios: true,
-        scenarioTimestamp: true,
-        launchReport: true,
-        metadata: {
-            "App Version":"0.0.1",
-            "Test Environment": "STAGING",
-            "Platform": "Windows 10",
-            "Executed": "Local"
-        }
-    };
+    theme: 'bootstrap',
+    jsonFile: 'test/cucumber-test-json/cucumber_report.json',
+    output: 'test/report/cucumber_report.html',
+    reportSuiteAsScenarios: true,
+    scenarioTimestamp: true,
+    launchReport: true,
+    metadata: {
+        "App Version":"0.0.1",
+        "Test Environment": "STAGING",
+        "Platform": "Windows 10",
+        "Executed": "Local"
+    }
+};
 
-    reporter.generate(options);
+reporter.generate(options);
