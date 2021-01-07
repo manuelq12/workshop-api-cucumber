dca2194a3490b20e063536e8ccbc85f0052ee294
Manuel Quintero Cubillos <mquinteroc@psl.com.co>
2021-01-07 10:06:49 -0500

    Static Code Analysis

--- test/features/support/MyFirstApiConsume/MyFirstApiConsumeGetSteps.js
+++ test/features/support/MyFirstApiConsume/MyFirstApiConsumeGetSteps.js
@@ -2,25 +2,24 @@ const agent = require('superagent');
 const { expect } = require('chai');
 const { When, Then } = require('cucumber');
 
-
 When('the user consumes a GET Service', async function () {
-    this.response = await agent.get(`${this.url}/ip`);
+  this.response = await agent.get(`${this.url}/ip`);
 });
 
-Then('the response must contain a property {string}', function(property) {
-    expect(this.response.body).to.have.property(property);
+Then('the response must contain a property {string}', function (property) {
+  expect(this.response.body).to.have.property(property);
 });
 
-When("the user consumes a GET Service with query parameters", async function ()  {
-    this.query = {
-        name: 'John',
-        age: '31',
-        city: 'New York'
-      };
+When('the user consumes a GET Service with query parameters', async function () {
+  this.query = {
+    name: 'John',
+    age: '31',
+    city: 'New York'
+  };
 
-    this.response = await agent.get(`${this.url}/get`).query(this.query);
+  this.response = await agent.get(`${this.url}/get`).query(this.query);
 });
 
-Then('the response must contain the query values', function (){
-    expect(this.response.body.args).to.eql(this.query);
+Then('the response must contain the query values', function () {
+  expect(this.response.body.args).to.eql(this.query);
 });
--- test/features/support/world/world.js
+++ test/features/support/world/world.js
@@ -1,12 +1,11 @@
 const { setWorldConstructor } = require('cucumber');
 
 class World {
-
   constructor() {
-    this.url= null;
+    this.url = null;
     this.response = null;
     this.query = null;
   }
-};
+}
 
 setWorldConstructor(World);
