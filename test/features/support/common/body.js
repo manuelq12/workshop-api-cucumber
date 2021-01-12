65fb9e5d4f5cf8ae80ac3b32c6e6445a221cf26f
Manuel Quintero Cubillos <mquinteroc@psl.com.co>
2021-01-12 16:38:48 -0500

    Added empty rule and statusCode without switch

--- test/features/support/common/body.js
+++ test/features/support/common/body.js
@@ -2,6 +2,5 @@ const { expect } = require('chai');
 const { Then } = require('cucumber');
 
 Then('the response body must be empty', function () {
-  /* eslint-disable no-unused-expressions */
   expect(this.response.body).to.be.empty;
 });
--- test/features/support/common/statusCode.js
+++ test/features/support/common/statusCode.js
@@ -2,14 +2,5 @@ const { expect } = require('chai');
 const { Then } = require('cucumber');
 
 Then('the response should contain a {int} status code', function (statusCodeExpected) {
-  switch (statusCodeExpected) {
-    case 200:
-      expect(this.response.status).to.equal(statusCodeExpected);
-      break;
-    case 204:
-      expect(this.response.status).to.equal(statusCodeExpected);
-      break;
-    default:
-      break;
-  }
+  expect(this.response.status).to.equal(statusCodeExpected);
 });
